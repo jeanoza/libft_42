93806c0e6c9636f32ae1718d48133a8666d7949b
Kyubong CHOI <kychoi@e1r2p19.42.fr>
2021-11-23 13:43:15 +0100

    feat(ft_strrchr)

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,12 +6,12 @@
 /*   By: kychoi <kychoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 19:36:15 by kychoi            #+#    #+#             */
-/*   Updated: 2021/11/23 09:51:56 by kychoi           ###   ########.fr       */
+/*   Updated: 2021/11/23 13:34:56 by kychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+//TODO:convert to unsigned int or size_t(with typedef)
 int	ft_strlen(const char *s)
 {
 	int	i;
--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -1,40 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kychoi <kychoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 12:02:16 by kychoi            #+#    #+#             */
-/*   Updated: 2021/11/23 13:41:27 by kychoi           ###   ########.fr       */
+/*   Created: 2021/11/23 12:59:19 by kychoi            #+#    #+#             */
+/*   Updated: 2021/11/23 13:42:04 by kychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	i = 0;
-	while (*(s + i) != c)
-	{
-		++i;
-		if (i > ft_strlen(s))
-			return (0);
-	}
+	i = ft_strlen(s);
+	while (*(s + i) != c && i >= 0)
+		--i;
+	if (i == -1)
+		return (0);
 	return ((char *)(s + i));
 }
+/*
+#include <stdio.h>
 #include <string.h>
 int	main(int ac, char **av)
 {
 	(void)ac;
-	printf("ft_strchr	:[%p]%s\n", ft_strchr(av[1], av[2][0]), ft_strchr(av[1], av[2][0]));
-	printf("strchr		:[%p]%s\n", strchr(av[1], av[2][0]), strchr(av[1], av[2][0]));
+	printf("ft_strrchr:	[%p]%s\n", ft_strrchr(av[1], av[2][0]), ft_strrchr(av[1], av[2][0]));
+	printf("strrchr:	[%p]%s\n", strrchr(av[1], av[2][0]), strrchr(av[1], av[2][0]));
 	//for test null
-	printf("\ntest with\n");
-	printf("ft_strchr	:[%p]%s\n", ft_strchr(av[1], '\0'), ft_strchr(av[1], '\0'));
-	printf("strchr		:[%p]%s\n", strchr(av[1], '\0'), strchr(av[1], '\0'));
+	printf("\ntest with \0\n");
+	printf("ft_strrchr:	[%p]%s\n", ft_strrchr(av[1], '\0'), ft_strrchr(av[1], '\0'));
+	printf("strrchr:	[%p]%s\n", strrchr(av[1], '\0'), strrchr(av[1], '\0'));
 	return (0);
 }
+*/
